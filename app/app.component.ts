90260c89c6233c7785dc6e7083eaf5d81027c9a7
Dan Ward <daniel.ward@forrent.com>
2016-09-19 17:30:04 -0400

    Move hero detail component to own class

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
 
-export class Hero {
-    id: number;
-    name: string;
-}
-
-
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
     selector: 'my-app',
@@ -18,14 +14,8 @@ export class Hero {
             </li>
         </ul>
 
-        <div *ngIf="selectedHero">
-            <h2>{{selectedHero.name}} deatils:</h2>
-            <div><label>id: </label>{{selectedHero.id }}</div>
-            <div>
-                <label>name: </label>
-                <input [(ngModel)]="selectedHero.name" placeholder="name">
-            </div>
-        </div>
+        <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+
     `,
     styles: [`
         .selected {
