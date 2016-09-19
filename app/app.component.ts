4741a812f4a4740a667c2bba2e5dc31a6a32b8a8
Dan Ward <daniel.ward@forrent.com>
2016-09-19 17:06:48 -0400

    First use of ngFor, ngIf and class property binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,19 +11,94 @@ export class Hero {
     selector: 'my-app',
     template: `
         <h1>{{title}}</h1>
-        <h2>{{hero.name}} deatils:</h2>
-        <div><label>id: </label>{{hero.id }}</div>
-        <div>
-            <label>name: </label>
-            <input [(ngModel)]="hero.name" placeholder="name">
+
+        <ul class="heroes">
+            <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
+                <span class="badge">{{hero.id}}</span> {{hero.name}}
+            </li>
+        </ul>
+
+        <div *ngIf="selectedHero">
+            <h2>{{selectedHero.name}} deatils:</h2>
+            <div><label>id: </label>{{selectedHero.id }}</div>
+            <div>
+                <label>name: </label>
+                <input [(ngModel)]="selectedHero.name" placeholder="name">
+            </div>
         </div>
-    `
+    `,
+    styles: [`
+        .selected {
+            background-color: #CFD8DC !important;
+            color: white;
+        }
+        .heroes {
+            margin: 0 0 2em 0;
+            list-style-type: none;
+            padding: 0;
+            width: 15em;
+        }
+        .heroes li {
+            cursor: pointer;
+            position: relative;
+            left: 0;
+            background-color: #EEE;
+            margin: .5em;
+            padding: .3em 0;
+            height: 1.6em;
+            border-radius: 4px;
+        }
+        .heroes li.selected:hover {
+            background-color: #BBD8DC !important;
+            color: white;
+        }
+        .heroes li:hover {
+            color: #607D8B;
+            background-color: #DDD;
+            left: .1em;
+        }
+        .heroes .text {
+            position: relative;
+            top: -3px;
+        }
+        .heroes .badge {
+            display: inline-block;
+            font-size: small;
+            color: white;
+            padding: 0.8em 0.7em 0 0.7em;
+            background-color: #607D8B;
+            line-height: 1em;
+            position: relative;
+            left: -1px;
+            top: -4px;
+            height: 1.8em;
+            margin-right: .8em;
+            border-radius: 4px 0 0 4px;
+        }
+    `]
+
 })
 
 export class AppComponent {
+    heroes = HEROES;
     title = 'Tour of Heroes';
-    hero: Hero = {
-        id: 1,
-        name: 'John Snow'
+    selectedHero: Hero;
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
     }
+
 }
+
+const HEROES: Hero[] = [
+    { id: 11, name: 'Inugo Montoya' },
+    { id: 12, name: 'Ugetsu' },
+    { id: 13, name: 'Agharta' },
+    { id: 14, name: 'Orishi' },
+    { id: 15, name: 'Ogun' },
+    { id: 16, name: 'Shango' },
+    { id: 17, name: 'Yemaja' },
+    { id: 18, name: 'Olokun' },
+    { id: 19, name: 'Oshu' },
+    { id: 20, name: 'Agayu' },
+];
